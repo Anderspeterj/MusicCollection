5dbef8f1c5dc1ab5480b92f1b46b00b1688945d2
Anders Peter Jørgensen <ande9001@edu.easj.dk>
2023-03-21 10:01:31 +0100

    Add project files.

---
+++ MusicCollection/Controller/RecordController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicCollection.Controller;
+using MusicCollection.Models;
+
+
+
+namespace MusicCollection.Controller 
+{
+
+    [Route("api/controller")]
+    [ApiController]
+    public class RecordController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MusicCollection/Models/Record.cs
@@ -0,0 +1,11 @@
+﻿namespace MusicCollection.Models
+{
+    public class Record
+    {
+        private string _title { get; set; }
+        private string _artist { get; set; }
+        private int _duration { get; set; }
+        private int _publicationYear { get; set; }   
+
+    }
+}
